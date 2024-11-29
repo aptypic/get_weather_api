e01ead09163b53841f94afa7878fd7ef0113d965
asivolap <artur.sivolap@haiku.dev>
2024-11-29 16:38:26 +0300

    collect link by urljoin

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
+from urllib.parse import urljoin
 import requests
-
 
 def fetch_weather(city):
     payload = {
         'nTqM': '',
         "lang": "ru"
     }
-    url = f'https://wttr.in/{city}'
+    url = urljoin('https://wttr.in/', f'/{city}')
     response = requests.get(url, params=payload)
     response.raise_for_status()
     return response.text
